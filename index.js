0fbbf3d576d25d8ff5b02d6e85f056f25437f977
James Sumners <james@sumners.email>
2022-12-04 13:02:39 -0500

    Initial commit
    Implements a baseline set of message types such that we cover all
    parsing paths in unit tests.

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+  LdapMessage: require('./lib/ldap-message'),
+
+  AbandonRequest: require('./lib/messages/abandon-request'),
+  BindRequest: require('./lib/messages/bind-request')
+}
