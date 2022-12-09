150059d4b0f8ec269727c5de58ff086e20c99851
James Sumners <james@sumners.email>
2022-12-09 11:41:25 -0500

    Add AddRequest

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = {
   LdapMessage: require('./lib/ldap-message'),
 
   AbandonRequest: require('./lib/messages/abandon-request'),
+  AddRequest: require('./lib/messages/add-request'),
   BindRequest: require('./lib/messages/bind-request'),
   DeleteRequest: require('./lib/messages/delete-request')
 }
