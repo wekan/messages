c7555d89dcb3ba76b40ecac95dd0b3fe382ca200
James Sumners <james@sumners.email>
2022-12-18 10:01:08 -0500

    Add LdapResult and AddResponse objects

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ module.exports = {
   AbandonRequest: require('./lib/messages/abandon-request'),
   AddRequest: require('./lib/messages/add-request'),
   BindRequest: require('./lib/messages/bind-request'),
-  DeleteRequest: require('./lib/messages/delete-request')
+  DeleteRequest: require('./lib/messages/delete-request'),
+
+  AddResponse: require('./lib/messages/add-response')
 }
