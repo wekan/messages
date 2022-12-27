ffda79eea1dff2774e9754253cb413d2da799201
James Sumners <james@sumners.email>
2022-12-27 09:29:20 -0500

    Add ExtensionRequest and ExtensionResponse objects
    We additionally include implementations for the examples listed at
    https://ldapwiki.com/wiki/Extended%20Request. Specifically:
    
    + Cancel
    + Password Modify
    + StartTLS
    + Who Am I?
    
    It should be noted, that we do not think the original extended
    request implementation in ldapjs accurately supported any
    extended request beyond basic string named and valued requests.

--- index.js
+++ index.js
@@ -8,10 +8,16 @@ module.exports = {
   BindRequest: require('./lib/messages/bind-request'),
   CompareRequest: require('./lib/messages/compare-request'),
   DeleteRequest: require('./lib/messages/delete-request'),
+  ExtensionRequest: require('./lib/messages/extension-request'),
 
   AbandonResponse: require('./lib/messages/abandon-response'),
   AddResponse: require('./lib/messages/add-response'),
   BindResponse: require('./lib/messages/bind-response'),
   CompareResponse: require('./lib/messages/compare-response'),
-  DeleteResponse: require('./lib/messages/delete-response')
+  DeleteResponse: require('./lib/messages/delete-response'),
+  ExtensionResponse: require('./lib/messages/extension-response'),
+
+  // Specific extension response implementations.
+  PasswordModifyResponse: require('./lib/messages/extension-responses/password-modify'),
+  WhoAmIResponse: require('./lib/messages/extension-responses/who-am-i')
 }
