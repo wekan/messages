9a6bc5048087d3201dfdd49ff5828fc2f6171ff8
James Sumners <james@sumners.email>
2023-02-16 10:38:22 -0500

    Add SearchRequest, SearchResult{Done,Entry,Reference}, and IntermediateResponse

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 'use strict'
 
 module.exports = {
+  // Base objects.
   LdapMessage: require('./lib/ldap-message'),
+  LdapResult: require('./lib/ldap-result'),
 
+  // Request objects.
   AbandonRequest: require('./lib/messages/abandon-request'),
   AddRequest: require('./lib/messages/add-request'),
   BindRequest: require('./lib/messages/bind-request'),
@@ -11,8 +14,10 @@ module.exports = {
   ExtensionRequest: require('./lib/messages/extension-request'),
   ModifyRequest: require('./lib/messages/modify-request'),
   ModifyDnRequest: require('./lib/messages/modifydn-request'),
+  SearchRequest: require('./lib/messages/search-request'),
   UnbindRequest: require('./lib/messages/unbind-request'),
 
+  // Response objects.
   AbandonResponse: require('./lib/messages/abandon-response'),
   AddResponse: require('./lib/messages/add-response'),
   BindResponse: require('./lib/messages/bind-response'),
@@ -22,7 +27,14 @@ module.exports = {
   ModifyResponse: require('./lib/messages/modify-response'),
   ModifyDnResponse: require('./lib/messages/modifydn-response'),
 
+  // Search request messages.
+  SearchResultEntry: require('./lib/messages/search-result-entry'),
+  SearchResultReference: require('./lib/messages/search-result-reference'),
+
   // Specific extension response implementations.
   PasswordModifyResponse: require('./lib/messages/extension-responses/password-modify'),
-  WhoAmIResponse: require('./lib/messages/extension-responses/who-am-i')
+  WhoAmIResponse: require('./lib/messages/extension-responses/who-am-i'),
+
+  // Miscellaneous objects.
+  IntermediateResponse: require('./lib/messages/intermediate-response')
 }
